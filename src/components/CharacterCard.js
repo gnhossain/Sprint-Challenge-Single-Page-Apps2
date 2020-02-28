65a4ab76c8b25b24df5c850d57265d5cf059d4e7
G H <gh@Gs-MacBook-Pro.local>
2020-02-28 09:39:36 -0800

    character card fixed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -12,12 +12,12 @@ export default function CharacterCard(props) {
   return (
         <div>
           <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+            <CardImg top width="100%" src={props.imgee} alt="Card image cap" />
             <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              <Button>Button</Button>
+              <CardTitle>Name:{props.name}</CardTitle>
+              <CardSubtitle>Status:{props.status}</CardSubtitle>
+              <CardText>Gender:{props.gender}</CardText>
+              {/* <Button>Button</Button> */}
             </CardBody>
           </Card>
         </div>
