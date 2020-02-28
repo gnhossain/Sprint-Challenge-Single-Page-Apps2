b68e68fe3a0f7cfff44fd8ddb0cb826b5cb91333
G H <gh@Gs-MacBook-Pro.local>
2020-02-28 09:57:24 -0800

    add route to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from 'react-router-dom';
+import CharacterList from './components/CharacterList';
+import WelcompePage from './components/WelcomePage.js';
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path='/' component={WelcompePage}/>
+      <Route path='/character' component={CharacterList}/>
+
+      
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import React from 'react';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
