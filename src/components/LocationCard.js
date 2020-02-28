45d304a155c43a4fded7592624ae4b177ecac83f
G H <gh@Gs-MacBook-Pro.local>
2020-02-28 12:26:37 -0800

    location

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -12,7 +12,7 @@ export default function LocationCard(props) {
   <Card>
     {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
     <CardBody>
-      <CardTitle>Name:{props.name}</CardTitle>
+      <CardTitle>Location:{props.name}</CardTitle>
       <CardSubtitle>Type:{props.type}</CardSubtitle>
       {/* <CardText>URL:{props.url}</CardText> */}
       {/* <Button>Button</Button> */}
