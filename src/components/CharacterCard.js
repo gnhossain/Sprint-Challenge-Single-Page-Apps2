68d1a4fc321e7e10e61b441e46586bacaae61d7c
G H <gh@Gs-MacBook-Pro.local>
2020-02-28 10:30:59 -0800

    search form done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -11,7 +11,7 @@ export default function CharacterCard(props) {
   return (
         <div>
           <Card>
-            <CardImg top width="100%" src={props.imgee} alt="Card image cap" />
+            <CardImg top width="100%" src={props.image} alt="Card image cap" />
             <CardBody>
               <CardTitle>Name:{props.name}</CardTitle>
               <CardSubtitle>Status:{props.status}</CardSubtitle>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  
+  const characters = props.characters;
+  console.log(props);
+  const [name, setName] = useState('')
+  
+  const handleChanges = event => {
+    setName(event.target.value);
+    console.log(name);
+  };
+  const submitForm = event => {
+    event.preventDefalt();
+    console.log(name);
+
+    console.log(characters.filter(character => character.name === name));
+    props.setCharacters(characters.filter(character => character.name === name));
+  }
   return (
     <section className="search-form">
-     // Add a search form here
+     {/* // Add a search form here */}
+     Search Character by Name
+     <form onSubmit={submitForm}>
+       <label htmlFor='Name'>Name</label>
+       <input
+       id='name'
+       type='text'
+       name='name'
+       onChange={handleChanges}
+       value={name}
+       />
+       <button type='submit'>Search</button>
+     </form>
     </section>
   );
 }
